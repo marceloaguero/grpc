749886716d9a3546b1b3500eeaf99bff3fdeeb20
Marcelo Aguero <marceloaguero@gmail.com>
2018-10-05 16:52:31 -0300

    add SSL/TLS certificates and server

---
+++ examples/helloworld/serverssl/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/marceloaguero/grpc/examples/helloworld/server/pb"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port    = ":50052"
+	certCrt = "cert/server.crt"
+	certCsr = "cert/server.csr"
+	certKey = "cert/server.key"
+)
+
+type server struct{}
+
+func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{Message: "Hello " + req.Name}, nil
+}
+
+func (s *server) SayHelloAgain(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{Message: "Hello again " + req.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	// Create TLS credentials
+	creds, err := credentials.NewServerTLSFromFile(certCrt, certKey)
+	if err != nil {
+		log.Fatalf("could not load TLS keys: %v", err)
+	}
+
+	// Create an array of gRPC options with credentials
+	opts := []grpc.ServerOption{grpc.Creds(creds)}
+
+	s := grpc.NewServer(opts...)
+	pb.RegisterGreeterServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
